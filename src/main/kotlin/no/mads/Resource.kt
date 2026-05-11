73b16235942f704f6f33631aff1f867fa0f6a875
Mads Opheim <mads.opheim@gmail.com>
2026-05-11 20:54:09 +0200

    Tar bort denne testinga

--- src/main/kotlin/no/mads/Resource.kt
+++ src/main/kotlin/no/mads/Resource.kt
@@ -22,8 +22,6 @@ class Resource(
     @GET
     @Blocking
     fun finnTid(): List<LedigTid> {
-        println(System.getenv("password"))
-        println(secretFactory.getEpost())
         val aktuelleDatoer = secretFactory.getDatoer().split(" ").map {
             val splitted = it.split(",")
             val dato = LocalDate.parse(splitted[0], pattern)
